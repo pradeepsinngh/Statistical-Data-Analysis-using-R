8d44f45681e561505b6a1a86a06112388df1c208
Pradeep Singh <pdeepsingh91@gmail.com>
2018-03-07 23:06:06 -0800

    commit

---
+++ Chap 03 -- Probability distributions/H.W.2.R
@@ -0,0 +1,36 @@
+# H.W.2
+
+# load the data
+# HData <- read.table("hdata.txt", header = TRUE)
+HData <- read.table(file.choose(), header = TRUE)
+
+# If the SBP >= 140, label it as "High SBP", else "Normal SBP"
+SBP.H <- ifelse(HData$SBP >= 140, "High SBP", "Normal SBP")
+
+# Add SBP.H as a col to HData
+HData$SBP.H <- SBP.H
+
+# Factor SBP.H
+SBP.H <- factor(SBP.H)
+
+# Freq table for SBP.H
+freq_table <- table(SBP.H)
+
+# 2 X 2 Freq table for SBP Level and Gender
+freqtable <- table(HData$SBP.H, HData$GENDER)
+rfreq <- prop.table(freqtable, margin = 2) * 100
+round(rfreq, 3)
+
+# Pie chart
+pie(table(HData$SBP.H), col=c("blue", "red"),radius = .8, main = "Levels of Systolic Blood Pressure", 
+    font.main = 3)
+
+# Stacked bar charts
+barplot(table(HData$SBP.H, HData$GENDER), col=c("blue", "red"), legend = levels(SBP.H), 
+              main = "Systolic Blood Pressure level in Male & Female", font.main = 4)
+        
+
+# Clustered bar charts
+barplot(table(HData$SBP.H, HData$GENDER), col=c("blue", "red"), legend = levels(SBP.H), 
+        beside = TRUE, main = "Systolic Blood Pressure level in Male & Female", font.main = 4)
+
---
+++ Chap 03 -- Probability distributions/H.W.3.R
@@ -0,0 +1,45 @@
+# H.W.3
+
+# load the data
+#HData <- read.table("hdata.txt", header = TRUE)
+HData <- read.table(file.choose(), header = TRUE)
+
+# If the SBP >= 140, label it as "High SBP", else "Normal SBP"
+SBP.H <- ifelse(HData$SBP >= 140, "High SBP", "Normal SBP")
+
+# Add SBP.H as a col to HData
+HData$SBP.H <- SBP.H
+
+# Factor SBP.H
+SBP.H <- factor(SBP.H)
+
+par(mfrow=c(1,2))
+hist(HData$DBP[HData$SBP.H == "High SBP"])
+hist(HData$DBP[HData$SBP.H == "Normal SBP"])
+
+boxplot(HData$DBP~HData$SBP.H)
+
+summary(HData$DBP)
+summary(HData$DBP[HData$SBP.H == "High SBP"])
+summary(HData$DBP[HData$SBP.H == "Normal SBP"])
+
+DBP.H <- ifelse(HData$DBP >= 90, "High DBP", "Normal DBP")
+
+# Add SBP.H as a col to HData
+HData$DBP.H <- DBP.H
+
+# Factor SBP.H
+DBP.H <- factor(DBP.H)
+
+# Freq table for SBP.H
+freq_table <- table(DBP.H)
+
+# 2 X 2 Freq table for SBP Level and Gender
+freqtable <- table(HData$SBP.H, HData$DBP.H)
+rfreq <- prop.table(freqtable, margin = 2) * 100
+round(rfreq, 3)
+
+
+freqtable1 <- table(HData$SBP.H, HData$DBP.H)
+rfreq1 <- prop.table(freqtable, margin = 1) * 100
+round(rfreq1, 3)
