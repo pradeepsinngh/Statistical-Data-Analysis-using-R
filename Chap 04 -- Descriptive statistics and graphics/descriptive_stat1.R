706f6c7010fbc8c58989c40ca0604af5722aa5e6
Pradeep Singh <pdeepsingh91@gmail.com>
2018-03-08 09:41:19 -0800

    Update and rename H.W.2.R to descriptive_stat1.R

--- Chap 04 -- Descriptive statistics and graphics/descriptive_stat1.R
+++ Chap 04 -- Descriptive statistics and graphics/descriptive_stat1.R
@@ -1,7 +1,11 @@
-# H.W.2
+# Author: Pradeep Singh
+# Email: pradeepsingh7890@live.com
 
+#######################
+#      Problem 1      #
+#######################
 # load the data
-# HData <- read.table("hdata.txt", header = TRUE)
+# HData <- read.table("health_dataset.txt", header = TRUE)
 HData <- read.table(file.choose(), header = TRUE)
 
 # If the SBP >= 140, label it as "High SBP", else "Normal SBP"
@@ -19,7 +23,7 @@ freq_table <- table(SBP.H)
 # 2 X 2 Freq table for SBP Level and Gender
 freqtable <- table(HData$SBP.H, HData$GENDER)
 rfreq <- prop.table(freqtable, margin = 2) * 100
-round(rfreq, 3)
+round(rfreq, 3)            # round upto 3 decimal points
 
 # Pie chart
 pie(table(HData$SBP.H), col=c("blue", "red"),radius = .8, main = "Levels of Systolic Blood Pressure", 
@@ -29,7 +33,6 @@ pie(table(HData$SBP.H), col=c("blue", "red"),radius = .8, main = "Levels of Syst
 barplot(table(HData$SBP.H, HData$GENDER), col=c("blue", "red"), legend = levels(SBP.H), 
               main = "Systolic Blood Pressure level in Male & Female", font.main = 4)
         
-
 # Clustered bar charts
 barplot(table(HData$SBP.H, HData$GENDER), col=c("blue", "red"), legend = levels(SBP.H), 
         beside = TRUE, main = "Systolic Blood Pressure level in Male & Female", font.main = 4)
